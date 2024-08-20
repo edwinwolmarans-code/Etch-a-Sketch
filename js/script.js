f1f67ba452c14f07eff72d7d5dd5b1312bb50b04
Edwin <142680250+edwinwolmarans-code@users.noreply.github.com>
2024-08-20 21:10:17 +0200

    fix: fix grid to display 16 x 16 layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-//const WIDTH = window.innerWidth
-const WIDTH = 400;
+const WIDTH = 800;
 let numBlocks = 16;
 let dimensions = WIDTH/(numBlocks/2);
 
@@ -9,12 +8,9 @@ for (let i = 0; i < (numBlocks * numBlocks); i++) {
     const div = document.createElement("div");
     div.setAttribute("id", `${i}`);
     div.classList.add("section");
-    // div.style.width = `${dimensions}px`
-    // div.style.height = `${dimensions}px`
-    div.textContent = "hi";
+    div.style.width = `${100/numBlocks}%`
+    div.style.height = `${100/numBlocks}%`
+    div.textContent = `${i}`;
 
     container.appendChild(div)
   }
-
-// div.style.cssText = "border: 2px; border-color: black;";
-// div.style.backgroundColor = "blue"; 
