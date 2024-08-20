28c2e89d8232bfb88996bdb89e2321a9e8e5c962
Edwin <142680250+edwinwolmarans-code@users.noreply.github.com>
2024-08-20 22:23:07 +0200

    feat: add event listener to track mouse movements
    Background colour changes when the mouse hovers over a section of the grid

--- js/script.js
+++ js/script.js
@@ -6,11 +6,19 @@ const container = document.querySelector("#container");
 
 for (let i = 0; i < (numBlocks * numBlocks); i++) {
     const div = document.createElement("div");
-    div.setAttribute("id", `${i}`);
+    div.setAttribute("id", `S${i}`);
     div.classList.add("section");
-    div.style.width = `${100/numBlocks}%`
-    div.style.height = `${100/numBlocks}%`
+    div.style.width = `${100/numBlocks}%`;
+    div.style.height = `${100/numBlocks}%`;
     div.textContent = " ";
 
-    container.appendChild(div)
+    container.appendChild(div);
+    
   }
+
+for (let e = 0; e < (numBlocks * numBlocks); e++) {
+    const uniqueSection = document.querySelector(`#S${e}`)
+    uniqueSection.addEventListener('mouseover',(event) => {
+    console.log("touched");
+    uniqueSection.style.backgroundColor = "blue";
+})};
